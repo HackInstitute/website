2586d4e33ca4f36387512b5c33b13160939bb109
Jan Kus <jan@railslove.com>
2017-03-21 21:59:03 +0100

    added more content

--- source/assets/js/main.js
+++ source/assets/js/main.js
@@ -1,11 +1,17 @@
 ;(function(){
 
-			// Menu settings
-			$('#menuToggle, .menu-close').on('click', function(){
-				$('#menuToggle').toggleClass('active');
-				$('body').toggleClass('body-push-toleft');
-				$('#theMenu').toggleClass('menu-open');
-			});
+	// Menu settings
+	$('#menuToggle, .menu-close').on('click', function(){
+		$('#menuToggle').toggleClass('active');
+		$('body').toggleClass('body-push-toleft');
+		$('#theMenu').toggleClass('menu-open');
+	});
 
+	$("#theFooter").hide();
+
+	$("#footer-button").click(function(){
+	  $("#theFooter").slideToggle();
+	  return false;
+	});		
 
 })(jQuery)
